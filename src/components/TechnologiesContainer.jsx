8b5a503173d7d18a1ebf43f26c55af70dcbe6ca2
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-02-25 12:50:29 -0300

    dimensões

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -15,8 +15,6 @@ const technologies = [
     {id: 'html', name: 'HTML5', icon: <DiHtml5/>},
     {id: 'css', name: 'CSS3', icon: <DiCss3/>},
     {id: 'js', name: 'JavaScript', icon: <DiJsBadge/>},
-    {id: 'node', name: 'Node.js', icon: <DiNodejsSmall/>},
-    {id: 'mysql', name: 'MySQL', icon: <DiMysql/>},
     {id: 'react', name: 'React', icon: <DiReact/>},
 ];
 
