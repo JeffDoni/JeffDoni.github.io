bfdbd11b00ee2325bf31e993425a5a2231bca8a3
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-02-26 01:08:30 -0300

    posicionamentos

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,8 +5,8 @@ const AboutContainer = () => {
   <section className="about-container">
     <h2>Sobre</h2>
     <p>Eu sou um desenvolvedor front-end nascido e criado em Barbacena/MG e estou atualmente cursando o curso de desenvolvimento web da Trybe para me tornar um desenvolvedor fullstack. Estou sempre em busca de novas tecnologias e aprendizados para aprimorar minhas habilidades e me tornar um profissional completo na área.</p>
-    <p>Minha paixão por programação começou quando descobri a possibilidade de criar interfaces e páginas web incríveis que impactam a vida das pessoas. A partir desse momento, decidi me aprofundar na área e buscar uma formação sólida para me tornar um desenvolvedor fullstack.</p>
-    <p>Atualmente, como desenvolvedor front-end, tenho um conhecimento sólido em HTML, CSS e JavaScript, bem como em diversas bibliotecas e frameworks como React.</p>
+    <p>Minha paixão por programação começou quando descobri a possibilidade de criar interfaces e páginas web incríveis que impactam a vida das pessoas. A partir desse momento, decidi me aprofundar na área e buscar uma formação sólida para me tornar um desenvolvedor fullstack</p>
+    <p>Atualmente, como desenvolvedor front-end, tenho um conhecimento sólido em HTML, CSS e Javascript, bem como em diversas bibliotecas e frameworks como React.</p>
 
   </section>
   )
